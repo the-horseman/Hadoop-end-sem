2abf2a42a30c73a838bd6327175308c3eb4392c4
Aryan Rishi Lamba <aryanrishi20@gmail.com>
2021-06-17 10:12:06 +0530

    Create MyReducer.java

---
+++ Question1/MyReducer.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+
+public class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> 
+{
+	
+	// reducer will recieve key value pair as  ODD [ 85 131 993 491 539 985 413.....
+                                            // 	EVEN [ 392 870 240 888 184 494 704 996 408....
+	
+	public void reduce(Text key, Iterable<IntWritable> values, Context context)	throws IOException, InterruptedException
+	{
+		int sum = 0;
+		if(key.equals("PRIME"))
+		{
+			for (IntWritable value : values)
+			{
+				sum += value.get();
+			}
+		}
+		context.write(key,new IntWritable(sum));;
+	}
+}
